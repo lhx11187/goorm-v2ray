a874234c41351b559f3ac17af6ea50fc82967d16
Blackduck <lhx11187@qq.com>
2020-12-24 14:51:24 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -3,11 +3,11 @@
 mkdir ./v2ray
 cd v2ray
 rm -f v2ray
-wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ray
+wget https://github.com/lhx11187/goorm-v2ray/raw/master/v2ray
 #设置运行权限
 chmod +x v2ray
 rm -f v2ctl
-wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ctl
+wget https://github.com/lhx11187/goorm-v2ray/raw/master/v2ctl
 chmod +x v2ctl
 rm -f config.json
 wget https://github.com/lhx11187/goorm-v2ray/raw/master/config.json
